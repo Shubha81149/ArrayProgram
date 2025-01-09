71ed82876955577a6955686b1af7c7405a3cd7f4
Shubhashree Pradhan <131782352+Shubha81149@users.noreply.github.com>
2025-01-09 23:48:02 +0530

    Update

--- Array/src/com/array/Duplicate.java
+++ Array/src/com/array/Duplicate.java
@@ -19,22 +19,51 @@ public class Duplicate {
 //		}
 //	}
 
-	public static ArrayList<Integer> dupli(int[] a) {
+	// public static ArrayList<Integer> dupli(int[] a) {
 
-		ArrayList<Integer> l = new ArrayList<>();
+	// 	ArrayList<Integer> l = new ArrayList<>();
+
+	// 	for (int i = 0; i < a.length; i++) {
+
+	// 		for (int j = i + 1; j < a.length; j++) {
+
+	// 			if (a[i] == a[j]) {
+	// 				l.add(a[i]);
+	// 				break;
+	// 			}
+	// 		}
+	// 	}
+
+	// 	return l;
+
+	// }
+
+	// public static void main(String[] args) {
+
+	// 	int[] a = { 2, 1, 3, 2, 4, 5, 4, 1 };
+	// 	System.out.println(Arrays.toString(a));
+
+	// 	ArrayList<Integer> l = dupli(a);
+	// 	System.out.println(l);
+
+	// }
+
+	public static void dupli(int[] a) {
+
+		HashSet<Integer> hs = new LinkedHashSet<>();
 
 		for (int i = 0; i < a.length; i++) {
 
 			for (int j = i + 1; j < a.length; j++) {
 
 				if (a[i] == a[j]) {
-					l.add(a[i]);
+					hs.add(a[i]);
 					break;
 				}
 			}
 		}
 
-		return l;
+		System.out.println(hs);
 
 	}
 
@@ -43,8 +72,8 @@ public class Duplicate {
 		int[] a = { 2, 1, 3, 2, 4, 5, 4, 1 };
 		System.out.println(Arrays.toString(a));
 
-		ArrayList<Integer> l = dupli(a);
-		System.out.println(l);
+		 dupli(a);
+		
 
 	}
 
